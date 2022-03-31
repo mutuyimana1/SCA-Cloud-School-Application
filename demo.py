121a3f9980f91e7d589b45bf454f0a7b42825429
Mutuyimana Alice_219003365 <amutuyimana64@gmail.com>
2022-03-31 17:13:51 +0200

    python simple program

---
+++ demo.py
@@ -0,0 +1,33 @@
+#                    Simple python program
+
+# 1.progrm that display text
+print("Hello she code Africa! this the firts program, so I used vsCode as text editor instead of Jenkins sorry for that, Thank you!! ")
+
+# 2. program that add two number
+
+a= int( input(("please enter the first number")))
+b= int(input(("please enter the second number")))
+print (a+b)
+
+
+arr = []
+arr = [12, 3, 4, 15]
+ans = sum(arr)
+print ('Sum of the array is ',ans)
+
+# current time
+
+from datetime import datetime
+
+to_day = datetime.now()
+
+currentTime = to_day.strftime("%H:%M:%S")
+print("Current Time =", currentTime)
+
+
+
+
+
+
+
+
